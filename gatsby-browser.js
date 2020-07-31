9b905c5673fe3eeed407a5a862e1fb384aa54219
Artem Metelskyi <metelskyi88@gmail.com>
2020-07-31 14:07:02 -0400

    rehydration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
 import "typeface-inter"
 import "./src/styles/global.css"
+
+const ReactDOM = require("react-dom")
+
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
